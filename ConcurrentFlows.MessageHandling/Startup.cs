9056f432929dc10511bb7c7d9200f44a19484d49
Joshua Steward <ptsteward1@yahoo.com>
2021-05-31 05:16:10 -0700

    updated reistration method

--- ConcurrentFlows.MessageHandling/Startup.cs
+++ ConcurrentFlows.MessageHandling/Startup.cs
@@ -43,14 +43,15 @@ namespace ConcurrentFlows.MessageHandling
             services.AddSignalR().AddAzureSignalR("Endpoint=https://xxx.service.signalr.net;AccessKey=xxx;Version=1.0;");
             var connectionString = "Endpoint=sb://xxx.servicebus.windows.net/;SharedAccessKeyName=xxx;SharedAccessKey=xxx";
             var topic = "something";
-            services.AddMessenger(new[]
-            {
-                new ServicebusPublisher<EventMessage>(new TopicClient(connectionString, topic))
-            },
-            new List<Func<IServiceProvider, IPublisher<EventMessage>>>()
-            {
-                sp => new SampleHubPublisher(sp.GetRequiredService<IHubContext<SampleHub, ISampleHubClient>>())
-            });
+            services.AddMessenger(
+                publishers: new[]
+                {
+                    typeof(SampleHubPublisher)
+                },
+                instances: new[]
+                {
+                    new ServicebusPublisher<EventMessage>(new TopicClient(connectionString, topic))
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -77,24 +78,26 @@ namespace ConcurrentFlows.MessageHandling
     {
         public static void AddMessenger<TMessage>(
             this IServiceCollection services,
-            IEnumerable<IPublisher<TMessage>> publishers = null,
-            IEnumerable<Func<IServiceProvider, IPublisher<TMessage>>> publisherFactories = null)
+            IEnumerable<Type> publishers = null,
+            IEnumerable<IPublisher<TMessage>> instances = null,
+            IEnumerable<Func<IServiceProvider, IPublisher<TMessage>>> factories = null)
             where TMessage : class
         {
-            if ((publishers is null || !publishers.Any()) && (publisherFactories is null || !publisherFactories.Any()))
+            if ((publishers is null || !publishers.Any() || publishers.All(p => p.GetType() is IPublisher<TMessage>)) && 
+                (instances is null || !instances.Any()) && 
+                (factories is null || !factories.Any()))
                 throw new ArgumentException($"Must register at least one publisher for {typeof(TMessage).Name}");
 
-            publishers ??= Enumerable.Empty<IPublisher<TMessage>>();
-            publisherFactories ??= Enumerable.Empty<Func<IServiceProvider, IPublisher<TMessage>>>();
+            publishers ??= Enumerable.Empty<Type>();
+            instances ??= Enumerable.Empty<IPublisher<TMessage>>();
+            factories ??= Enumerable.Empty<Func<IServiceProvider, IPublisher<TMessage>>>();
 
             foreach (var publisher in publishers)
-            {
+                services.AddSingleton(typeof(IPublisher<TMessage>), publisher);
+            foreach (var publisher in instances)
                 services.AddSingleton(publisher);
-            }
-            foreach (var factory in publisherFactories)
-            {
+            foreach (var factory in factories)
                 services.AddSingleton(factory);
-            }
             services.AddSingleton<IMessenger<TMessage>, Messenger<TMessage>>();
             services.AddSingleton(typeof(IMessengerWriter<TMessage>), sp => sp.GetRequiredService<IMessenger<TMessage>>());
             services.AddHostedService<BackgroundMessenger<TMessage>>();
